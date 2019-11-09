c61f6b379ae3f871acb3c45f7de385a6f2f50161
Yusinto Ngadiman <yus@launchdarkly.com>
2019-11-08 22:09:14 -0800

    Fixed errors, warnings and upgrade major packages.

--- examples/async-provider/src/client/index.js
+++ examples/async-provider/src/client/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { hydrate } from 'react-dom';
+import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { asyncWithLDProvider } from 'launchdarkly-react-client-sdk';
 import App from '../universal/app';
@@ -7,7 +7,7 @@ import App from '../universal/app';
 (async () => {
   const LDProvider = await asyncWithLDProvider({ clientSideID: '59b2b2596d1a250b1c78baa4' });
 
-  hydrate(
+  render(
     <BrowserRouter>
       <LDProvider>
         <App />
--- examples/hoc/src/server/index.js
+++ examples/hoc/src/server/index.js
@@ -1,2 +1,9 @@
 const UniversalHotReload = require('universal-hot-reload').default;
-UniversalHotReload(require('../../webpack.config.server.js'), require('../../webpack.config.client.js'));
+
+// supply your own webpack configs
+const serverConfig = require('../../webpack.config.server.js');
+const clientConfig = require('../../webpack.config.client.js');
+
+// the configs are optional, you can supply either one or both.
+// if you omit say the server config, then your server won't hot reload.
+UniversalHotReload({ serverConfig, clientConfig });
