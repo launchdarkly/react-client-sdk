707bd0d0c2434417082a4494e6d8756c29d833d0
Jonathan Felchlin <jonathan@xgecko.com>
2020-10-14 11:52:48 -0700

    Fixing camelCaseKeys export (#44)

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ import asyncWithLDProvider from './asyncWithLDProvider';
 import withLDConsumer from './withLDConsumer';
 import useFlags from './useFlags';
 import useLDClient from './useLDClient';
-import camelCaseKeys from './utils';
+import { camelCaseKeys } from './utils';
 
 export { LDProvider, withLDProvider, withLDConsumer, useFlags, useLDClient, asyncWithLDProvider, camelCaseKeys };
