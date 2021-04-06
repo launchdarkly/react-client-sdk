f18da04ecbaf8af95c0d4c8253cce887016cb164
Yusinto Ngadiman <yusinto@gmail.com>
2021-04-06 16:36:18 -0700

    Update link-dev.sh
    Co-authored-by: Ben Woskow <48036130+bwoskow-ld@users.noreply.github.com>

--- link-dev.sh
+++ link-dev.sh
@@ -10,20 +10,17 @@ echo "===== Install prod dependencies"
 rm -rf node_modules
 npm install --production
 
-echo "===== Linking to async-provider"
-mkdir -p examples/async-provider/node_modules
-rm -rf examples/async-provider/node_modules/launchdarkly-react-client-sdk
-mkdir -p examples/async-provider/node_modules/launchdarkly-react-client-sdk/node_modules
-mkdir -p examples/async-provider/node_modules/launchdarkly-react-client-sdk/lib
-cp package.json examples/async-provider/node_modules/launchdarkly-react-client-sdk/package.json
-cp -r node_modules/ examples/async-provider/node_modules/launchdarkly-react-client-sdk/node_modules/
-cp -r lib/ examples/async-provider/node_modules/launchdarkly-react-client-sdk/lib/
+echo "===== Linking to examples"
+declare -a examples=("async-provider hoc")
 
-echo "===== Linking to hoc"
-mkdir -p examples/hoc/node_modules
-rm -rf examples/hoc/node_modules/launchdarkly-react-client-sdk
-mkdir -p examples/hoc/node_modules/launchdarkly-react-client-sdk/node_modules
-mkdir -p examples/hoc/node_modules/launchdarkly-react-client-sdk/lib
-cp package.json examples/hoc/node_modules/launchdarkly-react-client-sdk/package.json
-cp -r node_modules/ examples/hoc/node_modules/launchdarkly-react-client-sdk/node_modules/
-cp -r lib/ examples/hoc/node_modules/launchdarkly-react-client-sdk/lib/
+for example in "${examples[@]}"
+do
+  echo "===== Linking to $example example"
+  mkdir -p examples/${example}/node_modules
+  rm -rf examples/${example}/node_modules/launchdarkly-react-client-sdk
+  mkdir -p examples/${example}/node_modules/launchdarkly-react-client-sdk/node_modules
+  mkdir -p examples/${example}/node_modules/launchdarkly-react-client-sdk/lib
+  cp package.json examples/${example}/node_modules/launchdarkly-react-client-sdk/package.json
+  cp -r node_modules/ examples/${example}/node_modules/launchdarkly-react-client-sdk/node_modules/
+  cp -r lib/ examples/${example}/node_modules/launchdarkly-react-client-sdk/lib/
+done
