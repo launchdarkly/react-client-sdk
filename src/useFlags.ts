a1968cebc2789a3e924f30a4a83bc74d8dc4be4a
Umayah Abdennabi <abden003@gmail.com>
2022-07-27 17:00:03 -0700

    remove usage of any

--- src/useFlags.ts
+++ src/useFlags.ts
@@ -10,7 +10,7 @@ import context, { LDContext } from './context';
  *
  * @return All the feature flags configured in your LaunchDarkly project
  */
-const useFlags = <T extends LDFlagSet = any>(): T => {
+const useFlags = <T extends LDFlagSet = LDFlagSet>(): T => {
   const { flags } = useContext<LDContext>(context);
 
   return flags as T;
