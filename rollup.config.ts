9854a95a5e0299078300f346b80864f860501a5c
Yusinto Ngadiman <yusinto@gmail.com>
2023-07-11 12:31:51 -0700

    [sc193803] Rollup react sdk output (#107)
    This PR introduces rollup to bundle the react sdk output:
    
    1. The rollup config is in typescript. It shares the default
    tsconfig.json with the project hence these warnings of
    `rollup.config.ts` which can be safely ignored.
    2. We now support both cjs and esm.
    3. As above.
    4. Type definitions are also generated by rollup.
    
    
    ![Screenshot_2023-07-10_at_3_28_42_PM](https://github.com/launchdarkly/react-client-sdk-private/assets/1593077/14180ee9-18ee-4c1e-839f-bb2047b58215)
    
    ---------
    
    Co-authored-by: Yusinto Ngadiman <yus@launchdarkly.com>

---
+++ rollup.config.ts
@@ -0,0 +1,44 @@
+import dts from 'rollup-plugin-dts';
+import esbuild from 'rollup-plugin-esbuild';
+import filesize from 'rollup-plugin-filesize';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+
+const plugins = [resolve(), esbuild(), json(), terser(), filesize()];
+const external = /node_modules/;
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'lib/cjs/index.js',
+        format: 'cjs',
+        sourcemap: true,
+      },
+    ],
+    plugins,
+    external,
+  },
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'lib/esm/index.js',
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins,
+    external,
+  },
+  {
+    input: 'src/index.ts',
+    plugins: [dts(), json()],
+    output: {
+      file: 'lib/index.d.ts',
+      format: 'es',
+    },
+  },
+];
