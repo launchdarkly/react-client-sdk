814f2345b3cf972b417ef0f451e161515b06e25d
PC Drew <pdrew@pagerduty.com>
2026-02-16 13:35:29 -0700

    nope, delete again

--- src/asyncWithLDProvider.tsx
+++ src/asyncWithLDProvider.tsx
@@ -46,20 +46,6 @@ export default async function asyncWithLDProvider(config: AsyncProviderConfig) {
     error = e as Error;
   }
 
-
-  let val = "";
-  val = "a";
-  switch(val) {
-    case "a":
-      break;
-
-    case "b":
-      break;
-
-    default:
-      break;
-  }
-
   const initialFlags = options?.bootstrap && options.bootstrap !== 'localStorage' ? options.bootstrap : fetchedFlags;
 
   const LDProvider = ({ children }: { children: ReactNode }) => {
